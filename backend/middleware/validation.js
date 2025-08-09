ffa06a0bcf862b4f35e418b6907447ce4074c31f
KyPython <kyjahntsmith@gmail.com>
2025-08-09 01:36:35 -0400

    Create validation.js

---
+++ backend/middleware/validation.js
@@ -0,0 +1,16 @@
+const { validationResult } = require('express-validator');
+
+function validateRequest(validations) {
+  return async (req, res, next) => {
+    for (const validation of validations) {
+      await validation.run(req);
+    }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  };
+}
+
+module.exports = { validateRequest };
